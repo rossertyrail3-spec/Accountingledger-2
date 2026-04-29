0852c6c30c13736354260b4a65291cca550e7d9a
tyrailrosser <rossertyrail3@gmail.com>
2026-04-28 23:55:47 -0400

    Try and catch added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Main {
+
+    static ArrayList<transactions> transactions = new ArrayList<transactions>();
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+
+        try {
+            loadTransaction();
+            homeScreen();
+        } catch (IOException e) {
+            System.out.println("Error loading transaction file: " + e.getMessage());
+        }
+    }
+    public static void loadTransaction() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("transactions.csv"));
+
+        String line = reader.readLine(); // skip header liner
+
+        while ((line = reader.readLine())!=null) {
+            try {
+                String[] tokens = line.split("\\|");
+
+                LocalDate date = LocalDate.parse(tokens[0]);
+                LocalTime time = LocalTime.parse(tokens[1]);
+                String description = tokens[2];
+                String vendor = tokens[3];
+                double amount = Double.parseDouble(tokens[4]);
+
+                transactions transaction = new transactions(date, time, description, vendor, amount);
+                transactions.add(transaction);
+            } catch (Exception e) {
+                System.out.println("Skipping invalid line:" + line);
+            }
+            }
+
+            reader.close();
+        }
+    }
+
+
 }
--- src/main/java/transactions.java
+++ src/main/java/transactions.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class transactions {
+    private LocalDate date;
+    private LocalTime time;
+    private String Despriction;
+    private String Vendor;
+    private double amount;
+
+    public transactions(LocalDate date, LocalTime time, String despriction, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        Despriction = despriction;
+        Vendor = vendor;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getDespriction() {
+        return Despriction;
+    }
+
+    public void setDespriction(String despriction) {
+        Despriction = despriction;
+    }
+
+    public String getVendor() {
+        return Vendor;
+    }
+
+    public void setVendor(String vendor) {
+        Vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
